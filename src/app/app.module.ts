9d6a75414db8af7f42c089f4d17f755eba50ef6a
sheyla pariona <sheylapariona@gmail.com>
2018-02-22 16:17:40 -0300

    atualizacao content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CorpoComponent } from './corpo/corpo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CorpoComponent
   ],
   imports: [
     BrowserModule
